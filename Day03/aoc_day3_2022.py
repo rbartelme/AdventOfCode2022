9468ef450ac31d5879ea18ea6e7a7d04c5b9a5c3
rbartelme <r.p.bartelme@gmail.com>
2022-12-03 15:54:30 -0700

    union not producing output

--- Day03/aoc_day3_2022.py
+++ Day03/aoc_day3_2022.py
@@ -28,7 +28,7 @@ def splt_lst_to_set(lst_str_in : str):
     ----------
     lst_str_in : str
         Takes a str as an input list-ifies it and splits the list in half.
-    
+
     Returns
     -------
     u_of_s : str
@@ -38,17 +38,20 @@ def splt_lst_to_set(lst_str_in : str):
     half = len(lst_in)//2
     lsta = lst_in[half:]
     lstb = lst_in[:half]
-    a_st = set(lsta)
-    b_st = set(lstb)
-    return str(a_st.union(b_st))
- 
- smol_letters = list(map(chr, range(97, 123)))
- big_letters = list(map(chr, range(65, 91)))
- lst_of_letters = smol_letters + big_letters
- letter_vals = list(range(1, 53))
- 
- letter_pts_map = dict(zip(lst_of_letters, letter_vals)
- 
- ELF_PACKING_LIST = r"day03_input.txt"
- 
- 
+    bw_union = set(lsta) & set(lstb)
+    return bw_union
+
+smol_letters = list(map(chr, range(97, 123)))
+big_letters = list(map(chr, range(65, 91)))
+lst_of_letters = smol_letters + big_letters
+letter_vals = list(range(1, 53))
+
+letter_pts_map = dict(zip(lst_of_letters, letter_vals))
+
+ELF_PACKING_LIST = r"./day3_input.txt"
+
+ln_lst = parse_elf_guide(elf_text = ELF_PACKING_LIST)
+
+un_letters  = [splt_lst_to_set(ii) for ii in ln_lst]
+
+print(un_letters[2])
