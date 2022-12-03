9e79968f94df264c288687ce20889c2373fbc972
rbartelme <r.p.bartelme@gmail.com>
2022-12-02 17:59:28 -0700

    key val error in your score dict

--- Day02/aoc_day02.py
+++ Day02/aoc_day02.py
@@ -7,7 +7,7 @@ Ryan Bartelme
 
 # hashmaps for translating char into game strings
 opponent_choices = {"A": "Rock", "B": "Paper", "C": "Scissors"}
-your_choices = {"X": "Rock", "Y": "Paper", "C": "Scissors"}
+your_choices = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
 shape_score = {"Rock": 1, "Paper": 2, "Scissors": 3}
 match_score = {"Loss": 0, "Draw": 3, "Win": 6}
 
