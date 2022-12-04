07438769388f6c3725c433af37d08b1b18b0ea12
Ryan Bartelme <r.p.bartelme@gmail.com>
2022-12-03 17:41:17 -0700

    Update aoc_day3_2022.py
    added dict parser and summation for part 1 answer

--- Day03/aoc_day3_2022.py
+++ Day03/aoc_day3_2022.py
@@ -43,6 +43,34 @@ def splt_lst_to_set(lst_str_in : str):
     u_of_s = str(temp_ls[0])
     return u_of_s
 
+# generic parser for the dictionaries
+def generic_dict_parser(dct : dict, input_str : str):
+    """
+    Parameters
+    ----------
+    dct : dict
+        Dictionary to parse.
+    input_str : str
+        String to parse against Dictionary Keys
+    Returns
+    -------
+    match_val : str or int
+         Returns a string or an int depending on the type.
+    """
+    # initialize match_val
+    if input_str in list(dct):
+        if type(dct[input_str]) == str:
+            match_val = ""
+            match_val = dct[input_str]
+        elif type(dct[input_str]) == int:
+            match_val = 0
+            match_val = dct[input_str]
+        else:
+            print("Some weird type error occured")
+    else:
+        print("String {input_str} not found in keys of {dct}")
+    return match_val
+
 smol_letters = list(map(chr, range(97, 123)))
 big_letters = list(map(chr, range(65, 91)))
 lst_of_letters = smol_letters + big_letters
@@ -55,6 +83,10 @@ ELF_PACKING_LIST = r"./day3_input.txt"
 ln_lst = parse_elf_guide(elf_text = ELF_PACKING_LIST)
 
 un_letters  = [splt_lst_to_set(ii) for ii in ln_lst]
-print(ln_lst[2])
-print(un_letters[2])
-print(type(un_letters[2]))
+#print(ln_lst[2])
+#print(un_letters[2])
+#print(type(un_letters[2]))
+
+weight_score = [generic_dict_parser(dct = letter_pts_map, input_str = kk) for kk in un_letters]
+total_weight = sum(weight_score)
+print("Part 1 answer - The total weight priority queue is:", total_weight)
