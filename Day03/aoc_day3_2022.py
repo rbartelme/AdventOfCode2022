50e403aec79547b2fc73832753e8c040a02f7b21
rbartelme <r.p.bartelme@gmail.com>
2022-12-03 17:37:33 -0700

    finally produced a string out from set bitwise operator

--- Day03/aoc_day3_2022.py
+++ Day03/aoc_day3_2022.py
@@ -18,7 +18,7 @@ def parse_elf_guide(elf_text : str):
     """
     with open(elf_text) as f_l:
         lines = f_l.readlines()
-        lst_lst_lines = [i.split() for i in lines]
+        lst_lst_lines = [str(i.split()).replace("'", "") for i in lines]
     return lst_lst_lines
 
 # function to split a file line list into a set
@@ -34,12 +34,14 @@ def splt_lst_to_set(lst_str_in : str):
     u_of_s : str
         The union of the two split lists as sets.
     """
-    lst_in = list(lst_str_in)
+    lst_in = list(str(lst_str_in))
     half = len(lst_in)//2
     lsta = sorted(lst_in[half:])
     lstb = sorted(lst_in[:half])
     bw_union = set(j for j in lsta) & set(m for m in lstb)
-    return bw_union
+    temp_ls = list(bw_union)
+    u_of_s = str(temp_ls[0])
+    return u_of_s
 
 smol_letters = list(map(chr, range(97, 123)))
 big_letters = list(map(chr, range(65, 91)))
@@ -53,5 +55,6 @@ ELF_PACKING_LIST = r"./day3_input.txt"
 ln_lst = parse_elf_guide(elf_text = ELF_PACKING_LIST)
 
 un_letters  = [splt_lst_to_set(ii) for ii in ln_lst]
-
+print(ln_lst[2])
 print(un_letters[2])
+print(type(un_letters[2]))
