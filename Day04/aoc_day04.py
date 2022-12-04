10cbdba4efe3f5f8d66faa7dea7e3171d6387b5b
Ryan Bartelme <r.p.bartelme@gmail.com>
2022-12-04 08:08:28 -0700

    Update aoc_day04.py
    day4 test

---
+++ Day04/aoc_day04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# read in file and strip chr convert to list of ints
+ELF_WORK = r"day04_input.txt
+
+# main file parser
+def parse_elf_guide(elf_text : str):
+    """
+    Parameters
+    ----------
+    elf_txt : str
+        Takes a string with a file path to parse using with open.
+    Returns
+    -------
+    lst_lst_lines : list
+        Returns a list of lists from the file parsing to analyze with the hashmaps.
+    """
+    with open(elf_text) as f_l:
+        lines = f_l.readlines()
+        lst_lst_lines = [i.split() for i in lines]
+    return lst_lst_lines
+
+# test parser and char list output
+test_elf = parse_elf_guide(elf_text = ELF_WORK)
