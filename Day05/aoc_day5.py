02d971fffcb25fe0dc5383263ff54fade0d800ca
rbartelme <r.p.bartelme@gmail.com>
2022-12-07 08:16:23 -0700

    string split on in_file, ints like 10 into [1, 0]

--- Day05/aoc_day5.py
+++ Day05/aoc_day5.py
@@ -20,8 +20,22 @@ def in_stacks_moves(in_file: str):
     moves: list
         List of moves to make between the stacks.
     """
-    with open(in_file) as i_f:
-        stks, movs = [s.split('\n') for s in i_f.read().split('\n\n')]
+    stks, movs = [s.split('\n') for s in open(in_file).read().split("\n\n")]
     stacks = [[x for x in st if x!=' '][::-1] for st in map(list,zip(*[*stks])) if st[-1]!=' ']
     moves = [[int(y) for y in m if y.isdigit()] for m in movs]
     return stacks, moves
+
+
+def find_part1(cargo, instructions):
+    for moves, a, b in instructions: 
+        for _ in range(moves):
+            cargo[b-1].append(cargo[a-1].pop())
+    return ''.join(map(lambda x: x[-1], cargo))
+
+# solve part1
+DAY5_IN = f"day5_input.txt"
+stak_z, moo_vez = in_stacks_moves(in_file=DAY5_IN)
+print(stak_z[0])
+print(moo_vez[10])
+part1_answer = find_part1(cargo=stak_z, instructions=moo_vez)
+print("The answer to AoC day 5 part 1 is:", part1_answer)
